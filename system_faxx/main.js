4bbb64c5bfec2c32ac047bd56b5f59db40402157
Faxx <152618282+faxxcoders@users.noreply.github.com>
2024-06-25 09:15:24 +0700

    update main.js

--- system_faxx/main.js
+++ system_faxx/main.js
@@ -82,13 +82,13 @@ code = code?.match(/.{1,4}/g)?.join("-") || code;
       
 sock.ev.on("messages.upsert", async (chatUpdate) => {
 try {
-let mek = chatUpdate.messages[0];
+global.mek = chatUpdate.messages[0];
 if (!mek.message) return;
 mek.message = Object.keys(mek.message)[0] === "ephemeralMessage" ? mek.message.ephemeralMessage.message : mek.message;
 if (mek.key && mek.key.remoteJid === "status@broadcast") return;
 if (!sock.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
 if (mek.key.id.startsWith("BAE5") && mek.key.id.length === 16) return;
-let m = serialize(sock, mek, store);
+global.m = serialize(sock, mek, store);
 require("./faxx")(sock, m, chatUpdate, store);
 } catch (err) {
 console.log(err);
